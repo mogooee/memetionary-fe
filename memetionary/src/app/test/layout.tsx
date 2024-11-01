386a950fb32c658d35760b5a9008d93ff33ad013
mogooee <mogoo22@naver.com>
2024-11-01 19:40:02 +0900

    🐛 테스트 페이지 layout 수정

--- memetionary/src/app/test/layout.tsx
+++ memetionary/src/app/test/layout.tsx
@@ -1,7 +1,7 @@
 export default function TestLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid max-h-fit min-h-full w-fit grid-rows-[max-content] gap-4 bg-primary-50 text-center">
-      <div className="absolute top-[43%] h-16 w-16 animate-spin rounded-full border-t-2 border-primary-800" />
+      {children}
     </div>
   );
 }
